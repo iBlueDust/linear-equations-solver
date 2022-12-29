ca6a6b140d82f0d222b3638f8babe57ccb6ba7c6
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2022-12-29 21:33:50 +0800

    refactor: move styles out of /static
    Enables scss and further possible optimizations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,13 @@
+<!-- <script lang="ts">
+    import type { LayoutData } from './$types';
+    export let data: LayoutData;
+</script> -->
+
+<slot />
+
+<style global lang="scss">
+@import './reset.css';
+	
 input {
 	border: 1px solid black;
 	border-radius: 4px;
@@ -17,8 +27,10 @@ button, input[type=button], input[type=submit], input[type=reset] {
 	box-shadow: 2px 2px 0 rgba(0, 0, 0, 0.25);
 	border: 1px solid black;
 	border-radius: 4px;
+	cursor: pointer;
 }
 
 .material-symbols-outlined {
 	user-select: none;
 }
+</style>
